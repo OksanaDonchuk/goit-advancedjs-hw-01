e1160c45fc252123fb221f74ef7ad2eb6bed34bc
Oksana Donchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2023-11-29 21:03:55 +0200

    Add part 2, part 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const VIDEO_CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const getCurrentTime = function (eve) {
+  localStorage.setItem(VIDEO_CURRENT_TIME_KEY, JSON.stringify(eve.seconds));
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem(VIDEO_CURRENT_TIME_KEY)) || 0
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const LS_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(LS_KEY)) || {};
+const { email, message } = form.elements;
+reloadPage();
+
+function onInputData() {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(LS_KEY, JSON.stringify(dataForm));
+}
+
+function reloadPage() {
+  if (dataForm) {
+    email.value = dataForm.email ?? '';
+    message.value = dataForm.message ?? '';
+  }
+}
+
+function onFormSubmit(eve) {
+  eve.preventDefault();
+  console.log({ email: email.value, message: message.value });
+
+  if (email.value === '' || message.value === '') {
+    return alert('The fields are not filled. Please fill in!');
+  }
+
+  localStorage.removeItem(LS_KEY);
+  eve.currentTarget.reset();
+  dataForm = {};
+}
